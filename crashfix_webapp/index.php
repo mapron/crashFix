703fdddfb32a26e1684651f72851dcf60708338c
Vladimir Smirnov <v.smirnov@movavi.com>
2019-04-03 15:02:59 +0700

    Sync test
    [TECH]

--- crashfix_webapp/index.php
+++ crashfix_webapp/index.php
@@ -13,5 +13,6 @@ if( isset( $_SERVER['CRASHFIX_WEBAPP_DEBUG'] ) )
     defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);    
 }
 
+
 require_once($yii);
 Yii::createWebApplication($config)->run();
